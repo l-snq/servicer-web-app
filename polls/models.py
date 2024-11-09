1313f716b518d3b202dbf6c988af98790feae81c
lsnq <tastytothesupreme@gmail.com>
2024-11-09 10:15:22 -0700

    created the start of the database

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Jobs(models.Model):
+    job1 = models.CharField(max_length=200)
+    job2 = models.CharField(max_length=200)
+    job3 = models.CharField(max_length=200)
+    job4 = models.CharField(max_length=200)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    time_submitted = models.DateTimeField("date submitted")
+    jobs_offered = models.ForeignKey(Jobs, on_delete=models.CASCADE)
