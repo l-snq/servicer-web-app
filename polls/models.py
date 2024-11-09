c3dc7ae619ea86c1e4967aac38ff4ca7c41e4d2e
lsnq <tastytothesupreme@gmail.com>
2024-11-09 11:09:36 -0700

    changed settings.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -9,8 +12,17 @@ class Jobs(models.Model):
     job3 = models.CharField(max_length=200)
     job4 = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.job1
+
 
 class User(models.Model):
     name = models.CharField(max_length=200)
     time_submitted = models.DateTimeField("date submitted")
-    jobs_offered = models.ForeignKey(Jobs, on_delete=models.CASCADE)
+    jobs_offered = models.ForeignKey(Jobs, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def was_published_recently(self):
+        return self.time_submitted >= timezone.now() - datetime.timedelta(days=1)
