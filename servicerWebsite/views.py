7e09d1a0708af078522a5e20c1f3a4c75f4066f2
Paul Saunders <psaunder@MacBook-Pro.local>
2024-11-09 10:22:41 -0700

    Added auth, non-auth landing page files

--- servicerWebsite/views.py
+++ servicerWebsite/views.py
@@ -4,8 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Hi!!! You are at the polls view index.")
-    return render(
-        request,
-        "servicerWebsite/base.html",
-        {}
-    )
+    if request.user.is_authenticated:
+        return render(
+            request,
+            "servicerWebsite/auth_index.html",
+            {}
+        )
+    else:
+        return render(
+            request,
+            "servicerWebsite/non_auth_index.html",
+            {}
+        )
